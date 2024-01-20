cc8ee2747b13aa6e0f3c9cfdbc50f6e21db84be3
akshat <akshatbhatya@gmail.com>
2024-01-20 16:01:45 +0530

    add card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import "./Card.css"
 
-function Card() {
-  return (
-    <div className='card_parent'>
+let image_url="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_360/"
 
-    <div className="image">
-        <img src="" alt="" />
-    </div>
-    <div className="content">
-        <hr />
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
-        <h4></h4>
-    </div>
-      
-    </div>
-  )
+function Card({ singleProduct }) {
+    let { name, cloudinaryImageId, locality, areaName, costForTwo, cuisines ,avgRating} = singleProduct
+    return (
+        <div className='card_parent'>
+
+            <div className="image">
+                <img src={image_url+cloudinaryImageId} alt="" />
+            </div>
+            <div className="content">
+                <hr />
+                <h4>{name}</h4>
+                <h4>{locality}</h4>
+                <h4>{areaName}</h4>
+                <h4>{costForTwo}</h4>
+                <h4>{cuisines.join(", ")}</h4>
+                <h4>{avgRating}</h4>
+            </div>
+
+        </div>
+    )
 }
 
 export default Card
--- src/Components/Reasturant_card_parent/Reasturant.jsx
+++ src/Components/Reasturant_card_parent/Reasturant.jsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import "./Reasturant.css"
+import Card from '../Card/Card'
 
 let fetchUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 function Reasturant() {
-
+    let [product, setProduct] = useState([])
     useEffect(() => {
         let fetchData = async () => {
 
             try {
                 let data = await fetch(fetchUrl);
                 let response = await data.json()
-                console.log(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-            } catch (error) {
+
+                let responseData = response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+
+                let allProductArray = []
+                for (let i = 0; i < responseData.length; i++) {
+                    allProductArray.push(responseData[i].info);
+                }
+                setProduct(allProductArray)
+            }
+             catch (error) {
                 console.log(error);
 
             }
 
         }
         fetchData()
+
+        console.log(product);
     })
     return (
         <div className='reasturant_parent'>
+
+            {
+                product.map((singleProduct)=>{
+                    return <Card singleProduct={singleProduct}/>
+                })
+            }
 
         </div>
     )
