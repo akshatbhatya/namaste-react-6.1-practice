63c22bf510afde01ee285e83f97c0e680f4ecadf
akshat <akshatbhatya@gmail.com>
2024-01-20 15:41:27 +0530

    ad api to fetch data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import react from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 import App from "./App";
 
 
--- src/Components/Reasturant_card_parent/Reasturant.jsx
+++ src/Components/Reasturant_card_parent/Reasturant.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Reasturant.css"
+
+let fetchUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 function Reasturant() {
 
-    useState([])
-  return (
-    <div className='reasturant_parent'>
-      
-    </div>
-  )
+    useEffect(() => {
+        let fetchData = async () => {
+
+            try {
+                let data = await fetch(fetchUrl);
+                let response = await data.json()
+                console.log(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+            } catch (error) {
+                console.log(error);
+
+            }
+
+        }
+        fetchData()
+    })
+    return (
+        <div className='reasturant_parent'>
+
+        </div>
+    )
 }
 
 export default Reasturant
